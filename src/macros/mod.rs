9eeef966a1ca40f9fa5f26cb5d67656eaf6e174a
Hiromi ISHII <konn.jinro@gmail.com>
2018-08-30 16:12:03 +0900

    Macro for deriving referential operations

---
+++ src/macros/mod.rs
@@ -0,0 +1,57 @@
+#![allow(unknown_lints)]
+#![allow(unused_macros)]
+
+macro_rules! lift_binop_to_ref {
+    (impl $imp:ident, $method:ident for $t:ty where $($bd_type:ident : $bound:tt),*) => {
+        impl<'a, 'b, $($bd_type: $bound,)*> $imp<&'b $t> for &'a $t {
+            type Output = <$t as $imp>::Output;
+
+            fn $method(self, other: &$t) -> Self::Output {
+                $imp::$method(self.clone(), other.clone())
+            }
+        }
+    };
+}
+
+macro_rules! lift_unaop_to_ref {
+    (impl $imp:ident, $method:ident for $t:ty where $($bd_type:ident : $bound:tt),*) => {
+        impl<'a, $($bd_type: $bound,)*> $imp for &'a $t {
+            type Output = <$t as $imp>::Output;
+
+            fn $method(self) -> Self::Output {
+                $imp::$method(self.clone())
+            }
+        }
+    };
+}
+
+macro_rules! derive_assign_with {
+    (
+        impl
+        $imp:ident,
+        $method:ident as
+        $cls:ident,
+        $op:ident for
+        $t:ty where
+        $($bd_type:ident :
+         $bound:tt),*
+    ) => {
+        impl<$($bd_type: $bound,)*> $imp<$t> for $t {
+            fn $method(&mut self, other: $t) {
+                *self = $cls::$op(self.clone(), other);
+            }
+        }
+    };
+}
+
+macro_rules! lift_nums_to_ref {
+    (impl for $t:ty where $($bd_type:ident : $bound:tt),*) => {
+        lift_binop_to_ref!(impl Add, add for $t where $($bd_type : $bound),*);
+        derive_assign_with!(impl AddAssign, add_assign as Add, add for $t where $($bd_type : $bound),*);
+        lift_binop_to_ref!(impl Mul, mul for $t where $($bd_type : $bound),*);
+        derive_assign_with!(impl MulAssign, mul_assign as Mul, mul for $t where $($bd_type : $bound),*);
+        lift_binop_to_ref!(impl Sub, sub for $t where $($bd_type : $bound),*);
+        derive_assign_with!(impl SubAssign, sub_assign as Sub, sub for $t where $($bd_type : $bound),*);
+        lift_unaop_to_ref!(impl Neg, neg for $t where $($bd_type : $bound),*);
+    };
+}
