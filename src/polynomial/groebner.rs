92f9ea3c01815b7e2111d8d1445071546856aec0
Hiromi ISHII <konn.jinro@gmail.com>
2018-08-31 00:22:00 +0900

    Syzygy crieterion

--- src/polynomial/groebner.rs
+++ src/polynomial/groebner.rs
@@ -1,23 +1,46 @@
-use polynomial::Polynomial;
 use monomial::Monomial;
+use polynomial::Polynomial;
 use ring::*;
 use scalar::*;
 use std::ops::*;
 
 pub fn buchberger<P: Polynomial>(mut ideal: Vec<P>) -> Vec<P>
 where
-    Scalar<<P as Polynomial>::Coeff> : Mul<P, Output=P>,
+    Scalar<<P as Polynomial>::Coeff>: Mul<P, Output = P>,
     P::Coeff: Field,
 {
-    let mut pairs : Vec<_> = (0..ideal.len()).flat_map(move |a| (0..a).map(move |b| (a,b))).collect();
+    let mut pairs: Vec<_> = (0..ideal.len())
+        .flat_map(move |a| (0..a).map(move |b| (a, b)))
+        .collect();
     println!("pairs: {:?}", pairs);
     let mut n = ideal.len();
     while let Some((i, j)) = pairs.pop() {
-        let (f, g) = (ideal[i].clone(), ideal[j].clone());
-        let (lt_f, lt_g) = (f.lead_monom().unwrap(), g.lead_monom().unwrap());
-        if lt_f.lcm(lt_g) == lt_f * lt_g {
+        let (lt_f, lt_g) = (
+            &ideal[0].lead_monom().unwrap(),
+            &ideal[1].lead_monom().unwrap(),
+        );
+        let lcm_fg = lt_f.lcm(*lt_g);
+
+        // Primarity check
+        if lcm_fg == *lt_f * *lt_g {
             continue;
         }
+
+        // Syzygy test
+        let syz = ideal.iter().enumerate().any(|(l, h)| {
+            let [k1, l1] = [i.min(i), i.max(l)];
+            let [k2, l2] = [j.min(j), j.max(l)];
+            let distinct = pairs
+                .iter()
+                .all(|(k, o)| (*k, *o) != (k1, l1) && (*k, *o) != (k2, l2));
+            l != i && l != j && distinct && h.lead_monom().unwrap().divides(&lcm_fg)
+        });
+        if syz {
+            continue;
+        }
+
+        // S-test
+        let (f, g) = (ideal[i].clone(), ideal[j].clone());
         let (_, s) = f.spol(g).div_mod_polys(ideal.clone());
         if !s.is_zero() {
             ideal.push(s);
@@ -26,6 +49,6 @@ where
             }
             n += 1;
         }
-    };
+    }
     ideal
 }
