239ce916d72490918abb6256d01067111038087a
Hiromi ISHII <konn.jinro@gmail.com>
2018-09-02 05:09:26 +0900

    More testing

--- src/monomial/tests.rs
+++ src/monomial/tests.rs
@@ -11,8 +11,31 @@ fn mul_resp_ord<X: Monomial>(a: X, b: X, c: X) -> bool {
     }
 }
 
+fn one_is_unit<X: Monomial>(xs: X) -> bool {
+    xs * X::one() == xs && xs == X::one() * xs
+}
+
+fn mul_assoc<X: Monomial>(xs: X, ys: X, zs: X) -> bool {
+    xs * (ys * zs) == (xs * ys) * zs
+}
+
 fn positive<X: Monomial>(a: X) -> bool {
     a == X::one() || a > X::one()
+}
+
+fn lcm_divisible<X: Monomial>(xs: X, ys: X) -> bool {
+    let lcm = xs.lcm(ys);
+    xs.divides(&lcm) && ys.divides(&lcm)
+}
+
+fn lcm_least<X: Monomial>(xs: X, ys: X) -> bool {
+    let lcm = xs.lcm(ys).exponents();
+    (0..lcm.len()).all(|i| {
+        let mut decreased = lcm.clone();
+        decreased[i].1 -= 1;
+        let decreased = X::from_exponents(&decreased[0..]);
+        !(xs.divides(&decreased) && ys.divides(&decreased))
+    })
 }
 
 fn var_decreasing<X: Monomial>() -> bool {
@@ -106,30 +129,50 @@ check_monom!{
         fn mul_resp_ord(xs,ys,zs) -> bool;
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod lex2 = Lex2 for {
         fn mul_resp_ord(xs,ys,zs) -> bool;
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod lex3 = Lex3 for {
         fn mul_resp_ord(xs,ys,zs) -> bool;
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod lex4 = Lex4 for {
         fn mul_resp_ord(xs,ys,zs) -> bool;
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod lex5 = Lex5 for {
         fn mul_resp_ord(xs,ys,zs) -> bool;
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grevlex2 = Grevlex2 for {
@@ -137,6 +180,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grevlex3 = Grevlex3 for {
@@ -144,6 +191,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grevlex4 = Grevlex4 for {
@@ -151,6 +202,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grevlex5 = Grevlex5 for {
@@ -158,6 +213,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grlex2 = Grlex2 for {
@@ -165,6 +224,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grlex3 = Grlex3 for {
@@ -172,6 +235,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grlex4 = Grlex4 for {
@@ -179,6 +246,10 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 
     mod grlex5 = Grlex5 for {
@@ -186,5 +257,9 @@ check_monom!{
         fn positive(xs) -> bool;
         fn var_decreasing() -> bool;
         fn resp_total_deg(xs, ys) -> TestResult;
+        fn one_is_unit(xs) -> bool;
+        fn mul_assoc(xs, ys, zs) -> bool;
+        fn lcm_divisible(xs, ys) -> bool;
+        fn lcm_least(xs, ys) -> bool;
     }
 }
